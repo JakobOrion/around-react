fc31efbf5a242e0fb36e272d093e8836cbdb5726
JakobOrion <jakoborion@gmail.com>
2021-02-11 20:45:12 -0500

    move the common popup component for PopupWithForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import PopupWithForm from './PopupWithForm';
 
 function App() {
   return (
@@ -11,119 +12,109 @@ function App() {
         <Footer />
       </div>
 
-      <div className="popup popup_type_edit-avatar">
-        <div className="popup__container">
-          <h3 className="popup__title">Change profile picture</h3>
-          <button
-            type="button"
-            aria-label="Close"
-            className="popup__close"
-          ></button>
-          <form className="popup__form form__avatar-edit" name="editAvatar">
-            <input
-              aria-label="Image URL"
-              type="url"
-              className="form__input form__input_type_url"
-              name="avatar"
-              placeholder="Image link"
-              aria-required="true"
-              required
-            />
-            <span className="form__error" aria-live="polite"></span>
+      <PopupWithForm
+        name='edit-avatar'
+        title='Change profile picture'
+      >
+              <input
+                aria-label="Image URL"
+                type="url"
+                className="form__input form__input_type_url"
+                name="avatar"
+                placeholder="Image link"
+                aria-required="true"
+                required
+              />
+              <span className="form__error" aria-live="polite"></span>
 
-            <button type="submit" name="Save" className="form__submit">
-              Save
-            </button>
-          </form>
-        </div>
-      </div>
+              <button type="submit" name="Save" className="form__submit">
+                Save
+              </button>
+      </PopupWithForm>
 
-      <div className="popup popup_type_edit-profile">
-        <div className="popup__container">
-          <h3 className="popup__title">Edit profile</h3>
-          <button
-            type="button"
-            aria-label="Close"
-            className="popup__close"
-          ></button>
-          <form className="popup__form form__profile-edit" name="editProfile">
-            <input
-              aria-label="Name"
-              type="text"
-              className="form__input form__input_type_name"
-              name="name"
-              placeholder="Name"
-              minLength="2"
-              maxLength="40"
-              aria-required="true"
-              required
-            />
-            <span className="form__error" aria-live="polite"></span>
+      <PopupWithForm
+        name='edit-profile'
+        title='Edit profile'
+      >
+              <input
+                aria-label="Name"
+                type="text"
+                className="form__input form__input_type_name"
+                name="name"
+                placeholder="Name"
+                minLength="2"
+                maxLength="40"
+                aria-required="true"
+                required
+              />
+              <span className="form__error" aria-live="polite"></span>
 
-            <input
-              aria-label="About me"
-              type="text"
-              className="form__input form__input_type_description"
-              name="about"
-              placeholder="About me"
-              minLength="2"
-              maxLength="200"
-              aria-required="true"
-              required
-            />
-            <span className="form__error" aria-live="polite"></span>
+              <input
+                aria-label="About me"
+                type="text"
+                className="form__input form__input_type_description"
+                name="about"
+                placeholder="About me"
+                minLength="2"
+                maxLength="200"
+                aria-required="true"
+                required
+              />
+              <span className="form__error" aria-live="polite"></span>
 
-            <button type="submit" name="Save" className="form__submit">
-              Save
-            </button>
-          </form>
-        </div>
-      </div>
+              <button type="submit" name="Save" className="form__submit">
+                Save
+              </button>
+      </PopupWithForm>
+      
+      <PopupWithForm
+        name='add-card'
+        title='New place'
+      >
+              <input
+                aria-label="Title"
+                type="text"
+                className="form__input form__input_type_card-title"
+                name="name"
+                placeholder="Title"
+                minLength="1"
+                maxLength="30"
+                aria-required="true"
+                required
+              />
+              <span className="form__error" aria-live="polite"></span>
 
-      <div className="popup popup_type_add-card">
-        <div className="popup__container">
-          <h3 className="popup__title">New place</h3>
-          <button
-            type="button"
-            aria-label="Close"
-            className="popup__close"
-          ></button>
-          <form className="popup__form form__add-card" name="addCard">
-            <input
-              aria-label="Title"
-              type="text"
-              className="form__input form__input_type_card-title"
-              name="name"
-              placeholder="Title"
-              minLength="1"
-              maxLength="30"
-              aria-required="true"
-              required
-            />
-            <span className="form__error" aria-live="polite"></span>
+              <input
+                aria-label="Image URL"
+                type="url"
+                className="form__input form__input_type_url"
+                name="link"
+                placeholder="Image link"
+                aria-required="true"
+                required
+              />
+              <span className="form__error" aria-live="polite"></span>
 
-            <input
-              aria-label="Image URL"
-              type="url"
-              className="form__input form__input_type_url"
-              name="link"
-              placeholder="Image link"
-              aria-required="true"
-              required
-            />
-            <span className="form__error" aria-live="polite"></span>
+              <button
+                type="submit"
+                name="Create"
+                className="form__submit form__submit_disabled"
+                disabled
+              >
+                Create
+              </button>
+      </PopupWithForm>
 
-            <button
-              type="submit"
-              name="Create"
-              className="form__submit form__submit_disabled"
-              disabled
-            >
-              Create
-            </button>
-          </form>
-        </div>
-      </div>
+
+      <PopupWithForm
+        name='delete-card'
+        title='Are you sure?'
+      >
+              <button type="submit" className="form__submit form__submit-delete">
+                Yes
+              </button>
+      </PopupWithForm> 
+      
 
       <div className="popup popup_type_image">
         <div className="popup__container popup__container_figure">
@@ -136,22 +127,6 @@ function App() {
             <img className="popup__image" />
             <figcaption className="popup__image-title"></figcaption>
           </figure>
-        </div>
-      </div>
-
-      <div className="popup popup_type_delete-card">
-        <div className="popup__container">
-          <h3 className="popup__title">Are you sure?</h3>
-          <button
-            type="button"
-            aria-label="Close"
-            className="popup__close"
-          ></button>
-          <form className="popup__form form__delete-card" name="deleteCard">
-            <button type="submit" className="form__submit form__submit-delete">
-              Yes
-            </button>
-          </form>
         </div>
       </div>
 
